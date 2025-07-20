7ec60f4e19b7e7cc2972b9d242eb102992e41181
Dhananjay <sahoodhananjay161@gmail.com>
2025-07-20 10:09:22 +0530

    setup Tailwind CSS with custom themes

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my 3D Portfolio!</h1>
+      <h1 className='text-3xl underline'>Welcome to my 3D Portfolio!</h1>
     </div>
   )
 }
